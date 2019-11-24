3f07b868243fb4148d6a85e5d7b522f70a376d6c
DariaWojno <dariadrobniak@gmail.com>
2019-11-24 16:48:34 +0100

    Zajęcia 24.11

--- src/test/java/org/vistula/restassured/pet/PetControllerTest.java
+++ src/test/java/org/vistula/restassured/pet/PetControllerTest.java
@@ -1,10 +1,15 @@
 package org.vistula.restassured.pet;
 
+import io.restassured.response.ValidatableResponse;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.vistula.restassured.RestAssuredTest;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -31,16 +36,71 @@ public class PetControllerTest extends RestAssuredTest {
 
     @Test
     public void shouldCreateNewPet() {
-        JSONObject requestParams = new JSONObject();
-        requestParams.put("id", 4);
-        requestParams.put("name", "Wolf");
+        JSONObject requestParams = new JSONObject();                      /*żeby stworzyć jsona w klasie ocject */
+        int value = ThreadLocalRandom.current().nextInt(20,Integer.MAX_VALUE);
+        requestParams.put("id", value);                                       /*korzystamy z metody put*/
+        requestParams.put("name", RandomStringUtils.randomAlphabetic(10));
 
-        given().header("Content-Type", "application/json")
+        given().header("Content-Type", "application/json")          /*w jednym teście stworzył i usunął nowe zwierzątko*/
                 .body(requestParams.toString())
                 .post("/pet")
                 .then()
                 .log().all()
                 .statusCode(201);
+        given().delete("/pet/"+value)
+                .then()
+                .log().all()
+                .statusCode(204);
+    }
+
+    @Test
+    public void shouldGetNotExistPet() {
+        int statusCode = given().get("/pet/7")
+                .then()
+                .log().all()
+                .statusCode(404)
+                .body(equalTo("There is no Pet with such id"))
+                .extract().statusCode();
+
+        assertThat(statusCode).isEqualTo(404);
+    }
+
+    @Test
+    public void shouldGetSecondPet() {
+        Object name = given().get("/pet/2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("id", is(2))
+                .body("name", equalTo("Dog"))
+                .extract().path("name");
+
+        assertThat(name).isEqualTo("Dog");
+
+
+    }
+
+    @Test
+    public void shouldGetSecondPetWithName() {
+        Pet pet = given().get("/pet/2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("id", is(2))
+                .body("name", equalTo("Dog"))
+                .extract().body().as(Pet.class);
+
+        assertThat(pet.getId()).isEqualTo(2);
+        assertThat(pet.getName()).isEqualTo("Dog");
+    }
+
+    @Test
+    public void shouldDeleteNewPet() {
+
+        given().delete("/pet/4")
+                .then()
+                .log().all()
+                .statusCode(204);
     }
 
 }
